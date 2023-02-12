fdee0faae2f040e0e50ef4166d6b32e0e0ba54c3
agaley <agaley@student.42lyon.fr>
2023-02-12 23:04:39 +0100

    Fix header

--- ft_printf.h
+++ ft_printf.h
@@ -6,17 +6,17 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 17:01:07 by agaley            #+#    #+#             */
-/*   Updated: 2023/02/12 01:39:36 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2023/02/12 20:37:36 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPRINTF_H
-# define LIBFTPRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
 # include "libft/libft.h"
 
-int ft_printf(const char *, ...);
+int	ft_printf(const char *format, ...);
 
 #endif
